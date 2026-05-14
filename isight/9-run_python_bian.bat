fcdab4fee8428d3644995be9f9eb05824d6e8960
zhudefu-tyut <asd1255244@163.com>
2026-05-14 09:19:50 +0800

    Update 9-run_python_bian.bat

--- isight/9-run_python_bian.bat
+++ isight/9-run_python_bian.bat
@@ -1,2 +1,2 @@
 @echo off
-C:\Users\gbl\AppData\Local\Programs\Python\Python39\python.exe "D:\Work_Directory_new\9-python-bian.py" 2>&1
+C:\Users\用户名\AppData\Local\Programs\Python\Python39\python.exe "源文件夹\9-python-bian.py" 2>&1
