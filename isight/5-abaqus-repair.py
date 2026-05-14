31dbd67e2363a7408455615abcd0d91f3e8d42f7
zhudefu-tyut <asd1255244@163.com>
2026-05-14 09:17:12 +0800

    Update 5-abaqus-repair.py

--- isight/5-abaqus-repair.py
+++ isight/5-abaqus-repair.py
@@ -1,34 +1,17 @@
-# -*- coding: mbcs -*-
-#
-# Abaqus/CAE Release 2024 replay file
-# Internal Version: 2023_09_21-20.55.25 RELr426 190762
-# Run by gbl on Thu Sep 25 16:46:42 2025
-#
+# Abaqus 导入切割模型并执行插件
 
-# from driverUtils import executeOnCaeGraphicsStartup
-# executeOnCaeGraphicsStartup()
-#: Executing "onCaeGraphicsStartup()" in the site directory ...
-from abaqus import *
-from abaqusConstants import *
-session.Viewport(name='Viewport: 1', origin=(0.0, 0.0), width=322.670806884766,
-    height=138.133331298828)
-session.viewports['Viewport: 1'].makeCurrent()
-session.viewports['Viewport: 1'].maximize()
-from caeModules import *
-from driverUtils import executeOnCaeStartup
-executeOnCaeStartup()
-session.viewports['Viewport: 1'].partDisplay.geometryOptions.setValues(
-    referenceRepresentation=ON)
-Mdb()
-#: A new model database has been created.
-#: The model "Model-1" has been created.
-session.viewports['Viewport: 1'].setValues(displayedObject=None)
-step = mdb.openStep('"D:\Work_Directory_new\model-cut.stp"', scaleFromFile=OFF)
-mdb.models['Model-1'].PartFromGeometryFile(name='model-cut', geometryFile=step,
-    combine=True, mergeSolidRegions=True, dimensionality=TWO_D_PLANAR,
-    type=DEFORMABLE_BODY)
-p = mdb.models['Model-1'].parts['model-cut']
-session.viewports['Viewport: 1'].setValues(displayedObject=p)
+初始化 Abaqus 环境
+创建新的模型数据库 Model-1
+设置并最大化视口
 
-execfile('D:/Work_Directory_new/abaqus_small_face_plugin.py', __main__.__dict__)
+// 1. 导入几何模型
+打开 STEP 文件：model-cut.stp
+基于几何文件创建二维可变形部件：
+    部件名称 = 'model-cut'
+    合并实体区域
 
+设置当前显示对象为 'model-cut' 部件
+
+// 2. 执行外部插件
+运行外部 Python 脚本：
+    execfile('abaqus_small_face_plugin.py')
