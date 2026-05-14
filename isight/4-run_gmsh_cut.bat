d30a4a4502331530391d55b4f15aa3cbb28d24ea
zhudefu-tyut <asd1255244@163.com>
2026-05-14 09:16:57 +0800

    Update 4-run_gmsh_cut.bat

--- isight/4-run_gmsh_cut.bat
+++ isight/4-run_gmsh_cut.bat
@@ -1,2 +1,2 @@
 @echo off
-C:\Users\gbl\AppData\Local\Programs\Python\Python39\python.exe "D:\Work_Directory_new\4-gmsh-cut.py" 2>&1
+C:\Users\用户名\AppData\Local\Programs\Python\Python39\python.exe "源文件夹\4-gmsh-cut.py" 2>&1
