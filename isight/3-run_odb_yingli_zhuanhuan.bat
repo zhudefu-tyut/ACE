90b443cedadb659766303b4d437e9c2503a70573
zhudefu-tyut <asd1255244@163.com>
2026-05-14 09:16:23 +0800

    Update 3-run_odb_yingli_zhuanhuan.bat

--- isight/3-run_odb_yingli_zhuanhuan.bat
+++ isight/3-run_odb_yingli_zhuanhuan.bat
@@ -1,3 +1,3 @@
 @echo off
-C:\Users\gbl\AppData\Local\Programs\Python\Python39\python.exe "D:\Work_Directory_new\3-python-odb1.py" 2>&1
-C:\Users\gbl\AppData\Local\Programs\Python\Python39\python.exe "D:\Work_Directory_new\3-python-odb2.py" 2>&1
+C:\Users\用户名\AppData\Local\Programs\Python\Python39\python.exe "源文件夹\3-python-odb1.py" 2>&1
+C:\Users\用户名\AppData\Local\Programs\Python\Python39\python.exe "源文件夹\3-python-odb2.py" 2>&1
